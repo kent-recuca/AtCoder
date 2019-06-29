098ada44edab9f8a80a441bcab4fb406fab23afa
DESKTOP-FOC1CC4\佐藤絢斗 <ase16410181@wiz.ac.jp>
2019-06-30 02:19:22 +0900

    small fix

--- AtCoder/ABC132/A-Fifty-Fifty.py
+++ AtCoder/ABC132/A-Fifty-Fifty.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 def main():
     S = list(input())
+    S.sort()
     SS = set(S)
-    if len(SS)==2:
+    if len(SS)==2 and S[0]==S[1] and S[2]==S[3]:
         print('Yes')
     else:
         print('No')
