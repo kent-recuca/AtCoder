e14639f12b31c1017d350c2667e741e433247e41
DESKTOP-FOC1CC4\佐藤絢斗 <ase16410181@wiz.ac.jp>
2019-06-30 19:36:47 +0900

    add code

---
+++ AtCoder/BeginnersSelection/ABC088B-Card-Game-for-Two.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+def main():
+    N = int(input())
+    A = [int(i) for i in input().split()]
+    A.sort()
+    A.reverse()
+    ans = 0
+    for i in range(N):
+        if i%2 == 0:
+            ans += A[i]
+        else:
+            ans -= A[i]
+    print(ans)
+
+if __name__ == '__main__':
+    main()
